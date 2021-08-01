1e6858e292277949025911ba73f4298cdb003e3c
alvkeke <alvkeke@126.com>
2021-08-01 15:44:39 +0800

    [update] structure of program was out of date, remove all old codes.

--- src/Server/BroadcastCallback.java
+++
@@ -1,8 +0,0 @@
-package Server;
-
-import java.net.InetAddress;
-
-public interface BroadcastCallback {
-    void gotClientOnline(String user, InetAddress address);
-    void gotClientOffline(String user);
-}
--- src/Server/FileRecvCallback.java
+++
@@ -1,7 +0,0 @@
-package Server;
-
-public interface FileRecvCallback {
-    boolean isCredible(String username);
-    void gotFile(String fileLocation);
-    void recvFileFailed(byte Reason, String param);
-}
